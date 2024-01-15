979cc17853060d86eabcb7ef34c6c93aa0c51239
Leah Jia <99011487+leahjia@users.noreply.github.com>
2024-01-14 22:14:48 -0800

    into props & iter

--- src/main.rs
+++ src/main.rs
@@ -20,17 +20,91 @@ fn App() -> impl IntoView {
         <br/>
         <ProgressBar progress=count/>
         <br/>
-        <ProgressBar progress=double_count/>
+        <ProgressBar progress=Signal::derive(double_count)/>
 
         <p> "Single Count: " {count} </p>
         <p> "Double Count: " {double_count} </p>
+
+
+        // iter
+        <br/>
+        <h3>"Static List"</h3>
+        <StaticList length=5/>
+
+        <h3>"Dynamic List"</h3>
+        <DynamicList length=5/>
     }
 }
 
 #[component]
-fn ProgressBar<F: Fn() -> i32 + 'static>(
+fn StaticList(length: usize) -> impl IntoView {
+    let counters = (1..=length).map(|idx| create_signal(idx));
+    let counter_btns = counters
+        .map(|(count, set_count)| {
+            view! {
+                <li>
+                    <button on:click=move |_| set_count.update(|n| *n += 1)>
+                    {count}
+                    </button>
+                </li>
+            }
+        })
+        .collect_view();
+    view! {
+        <ul>{counter_btns}</ul>
+    }
+}
+
+#[component]
+fn DynamicList(length: usize) -> impl IntoView {
+    let mut next_counter = length;
+    let init_counters = (0..length).map(|id| (id, create_signal(id + 1))).collect::<Vec<_>>();
+    let (counters, set_counters) = create_signal(init_counters);
+    let add_counter = move |_| {
+        // signal for new counter
+        let signal = create_signal(next_counter + 1);
+        // add new counter to counters
+        set_counters.update(move |counters| counters.push((next_counter, signal)));
+        next_counter += 1;
+    };
+
+    view! {
+        <div>
+            <button on:click=add_counter>
+               "Add Counter"
+            </button>
+            <ul>
+                <For
+                    each=counters
+                    key=|counter| counter.0
+                    children=move |(id, (count, set_count))| {
+                        view! {
+                            <li>
+                                <button on:click=move |_| set_count.update(|n| *n += 1)>
+                                    {count}
+                                </button>
+                                <button on:click=move |_| {
+                                    set_counters.update(|counters| {
+                                        counters.retain(|(counter_id, _)| counter_id != &id)
+                                    });
+                                }>
+                                    "Remove"
+                                </button>
+                            </li>
+                        }
+                    }
+                />
+            </ul>
+        </div>
+    }
+}
+
+#[component]
+fn ProgressBar(
+    // max val of the progress bar
     #[prop(default = 100)] max: u16,
-    progress: F
+    // curr progress to display
+    #[prop(into)] progress: Signal<i32>,
 ) -> impl IntoView {
     view! {
         <progress
