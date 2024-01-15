d4d9610d0707f6c61f8c13f4ad3a3a7f9f96cfa7
Leah Jia <99011487+leahjia@users.noreply.github.com>
2024-01-14 19:24:34 -0800

    generic

--- src/main.rs
+++ src/main.rs
@@ -7,44 +7,35 @@ fn main() {
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
-    let (x, set_x) = create_signal(0);
     let double_count = move || count() * 2;
-    let html = "<p>Some inner HTML stuff.</p>";
 
     view! {
-        <div inner_html=html/>
-
         <button
             on:click=move |_| set_count.update(|n| *n += 1)
             class=("btn-blue", move || count() % 2 == 1)
-
-            // style="position: absolute"
-            // style:left=move || format!("{}px", x())
-            // style:background-color=move || format!("rgb({}, {}, 100)", x(), 100)
-            // style:max-width="400px"
-            // style=("--columns", x)
-
             >
             "Click"
-            // {count}
         </button>
 
         <br/>
-
-        <progress
-            max="50"
-            // same as `value=move || count.get()`
-            value=count
-        />
-
+        <ProgressBar progress=count/>
         <br/>
-
-        <progress
-            max="50"
-            value=double_count
-        />
+        <ProgressBar progress=double_count/>
 
         <p> "Single Count: " {count} </p>
         <p> "Double Count: " {double_count} </p>
+    }
+}
+
+#[component]
+fn ProgressBar<F: Fn() -> i32 + 'static>(
+    #[prop(default = 100)] max: u16,
+    progress: F
+) -> impl IntoView {
+    view! {
+        <progress
+            max=max
+            value=progress
+        />
     }
 }
