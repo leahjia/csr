ac33d783ceda5b005da241d41fe94f0923d66dd4
Leah Jia <99011487+leahjia@users.noreply.github.com>
2024-01-16 14:28:42 -0800

    descriptive btns

--- src/callback.rs
+++ src/callback.rs
@@ -7,11 +7,65 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (toggled, set_toggled) = create_signal(false);
+    let (blue, set_blue) = create_signal(false);
+    let (right, set_right) = create_signal(false);
+    let (italics, set_italics) = create_signal(false);
+    let (lowercase, set_lowercase) = create_signal(false);
+
+    provide_context(set_lowercase);
+
     view! {
-        <p>"Toggle? " {toggled}</p>
-        <ButtonA setter=set_toggled/>
-        <ButtonB on_click=move |_| set_toggled.update(|val| *val = !*val)/>
+        <p
+            class:blue=blue
+            class:right=right
+            class:italics=italics
+            class:lowercase=lowercase
+            >"Whatever the message is"</p>
+        <ButtonA setter=set_blue/>
+        <ButtonB on_click=move |_| set_right.update(|val| *val = !*val)/>
+        <ButtonC on:click=move |_| set_italics.update(|val| *val = !*val)/>
+
+        <Layout/>
+    }
+}
+
+#[component]
+pub fn Layout() -> impl IntoView {
+    view! {
+        <header>
+            <h2>"through a layout"</h2>
+        </header>
+        <main>
+            <Content/>
+        </main>
+    }
+}
+
+#[component]
+pub fn Content() -> impl IntoView {
+    view! {
+        <div class="content">
+            <ButtonD/>
+        </div>
+    }
+}
+
+#[component]
+pub fn ButtonD() -> impl IntoView {
+    // search up the context tree for this signal
+    let setter = use_context::<WriteSignal<bool>>().expect("to have setter provided");
+
+    view! {
+        <button on:click=move |_| setter.update(|val| *val = !*val)>
+            "Toggle D: lowercase"
+        </button>
+    }
+}
+
+#[component]
+pub fn ButtonC() -> impl IntoView {
+    view! {
+        <button>"Toggle C: italics"</button>
     }
 }
 
@@ -21,7 +75,7 @@ where
     F: Fn(MouseEvent) + 'static
 {
     view! {
-        <button on:click=on_click>"Toggle B"</button>
+        <button on:click=on_click>"Toggle B: go right"</button>
     }
 }
 
@@ -31,7 +85,7 @@ pub fn ButtonA(setter: WriteSignal<bool>) -> impl IntoView {
         <button
             on:click=move |_| setter.update(|val| *val = !*val)
         >
-            "Toggle A"
+            "Toggle A: blue"
         </button>
     }
 }
